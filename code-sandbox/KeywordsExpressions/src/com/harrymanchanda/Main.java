0848b55d54c0127e12f6c719129ca9d01ecbaf26
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-11-26 16:05:21 +0530

    Feat: Keywords & Expressions!

---
+++ code-sandbox/KeywordsExpressions/src/com/harrymanchanda/Main.java
@@ -0,0 +1,14 @@
+package com.harrymanchanda;
+
+public class Main {
+    public static void main(String[] args) {
+        // https://en.wikipedia.org/wiki/List_of_Java_keywords
+        double kilometres = (100 * 1.609344);
+        System.out.println("Shoaib Akhtar just bowled " + kilometres + " km/hr");
+
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
